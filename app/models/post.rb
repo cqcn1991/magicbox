778d195f19eb8d432a363ebbb1a28e547ecac6ea
cqcn1991 <cqcn1991@gmail.com>
2014-10-02 19:11:24 +0800

    Display Created time

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :url, uniqueness: true
   scope :by_forum, ->(forum) { where source: forum }
+  default_scope{order('created_at DESC')}
 end
